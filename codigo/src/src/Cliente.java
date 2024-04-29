71ea8993e37ec24c10d00132e56ded924f71fa91
pedro porto <pedro20049955080800@gmail.com>
2024-04-29 17:37:30 -0300

    trocando nome de atributo
    autocliente para contador cliente

--- codigo/src/src/Cliente.java
+++ codigo/src/src/Cliente.java
@@ -1,12 +1,12 @@
 
 public class Cliente {
-    private static int autoCliente = 0;
+    private static int contadorCliente = 0;
     private int idCliente;
     private String nome;
     private int qtdPessoas;
 
     public Cliente(String nome, int qtdPessoas) {
-        this.idCliente = ++autoCliente;
+        this.idCliente = ++contadorCliente;
         this.nome = nome;
         this.qtdPessoas = qtdPessoas;
     }
