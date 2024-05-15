877b1a2846b96a03d6d7c1795d9ae1d3e93eead0
Vitor Rebula Nogueira <devrebula@gmail.com>
2024-05-14 21:42:53 -0300

    mudamos numero pra id na mesa

--- codigo/src/src/src/Mesa.java
+++ codigo/src/src/src/Mesa.java
@@ -2,12 +2,11 @@ package src;
 public class Mesa {
     private int capacidade;
     private boolean status;
-    private int numero;
 
     public Mesa(int capacidade, int numero) {
         this.capacidade = capacidade;
         this.status = false;
-        this.numero = numero;
+
     }
     public int getCapacidade() {
         return this.capacidade;
@@ -18,17 +17,29 @@ public class Mesa {
     public boolean getStatus() {
         return this.status;
     }
+<<<<<<< Updated upstream:codigo/src/src/src/Mesa.java
     public int getnumero() {
         return this.numero;
     }
 
     public void ocuparMesa() {
         this.status = true;
+=======
+
+    // OCUPAR e DESOCUPAR
+    public void ocuparMesa() {
+        this.status = true;
+        System.out.println("Por favor, direcione o cliente à mesa " + this.idMesa + "\n \n");
+>>>>>>> Stashed changes:codigo/src/src/Mesa.java
     }
     public void desocuparMesa() {
         this.status = false;
+<<<<<<< Updated upstream:codigo/src/src/src/Mesa.java
         System.out.println("Iremos buscar na fila de espera, caso não haja, a mesa ficará livre!");
         verificarFila();
+=======
+        System.out.println("Mesa " + this.idMesa + " desocupada com sucesso! \n \n");
+>>>>>>> Stashed changes:codigo/src/src/Mesa.java
     }
     public void verificarFila(){
         //não sei como faz p verificar a fila
