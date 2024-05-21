bc31b1e2d8447d77ffc929b021339313df57b572
Albert Luis Pereira de Jesus <albertluis123y88@gmail.com>
2024-05-21 16:25:20 -0300

    deu ruim

--- codigo/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Mesa m = new Mesa(8, 10);
-
-        System.out.println(m.getCapacidade());
-    }
-}
